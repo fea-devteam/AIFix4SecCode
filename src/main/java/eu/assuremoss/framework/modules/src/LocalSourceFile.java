6debffdca86c1751a13a706eaa57ce1eab5591b1
fea-devteam <selike20@gmail.com>
2022-07-21 11:51:16 +0200

    Copy project to temp folder if -singleFile argument is used

--- src/main/java/eu/assuremoss/framework/modules/src/LocalSourceFile.java
+++ src/main/java/eu/assuremoss/framework/modules/src/LocalSourceFile.java
@@ -5,9 +5,13 @@ import eu.assuremoss.framework.api.SourceCodeCollector;
 import eu.assuremoss.utils.PathHandler;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import static eu.assuremoss.utils.Configuration.PROJECT_PATH_KEY;
-import static eu.assuremoss.utils.Configuration.PROJECT_SOURCE_PATH_KEY;
+import static eu.assuremoss.utils.Configuration.*;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class LocalSourceFile implements SourceCodeCollector {
     private final String location;
@@ -24,11 +28,49 @@ public class LocalSourceFile implements SourceCodeCollector {
 
         String filePath = PathHandler.joinPath(projectAbsolutePath, projectSourceDir, location);
 
-        srcLocation = new File(filePath);
+        String newPath = copyToNewProject(filePath, projectSourceDir);
+
+        if (newPath != null) {
+            srcLocation = new File(newPath);
+        } else {
+            throw new RuntimeException("Provided location doesn't exist: " + filePath);
+        }
     }
 
     @Override
     public File getSourceCodeLocation() {
         return srcLocation;
+    }
+
+    private String copyToNewProject(String filePath, String projectSourceDir) {
+        String projectName = VulnRepairDriver.properties.getProperty(PROJECT_NAME_KEY);
+
+        Path file = Paths.get(filePath);
+
+        // Get the subfolder in which the file is in
+        Path locationParent = Paths.get(location).getParent();
+        String locationFolder = locationParent == null ? "" : locationParent.toString();
+
+        Path tempFolder = Paths.get("temp", projectName, projectSourceDir, locationFolder);
+
+        try {
+            Files.createDirectories(tempFolder);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        System.out.println("Moving " + projectName + " to a temp directory: " + tempFolder.toAbsolutePath());
+
+        Path tempFile = Paths.get(tempFolder.toString(), file.getFileName().toString());
+
+        try {
+            Files.copy(file, tempFile, REPLACE_EXISTING);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return tempFile.toAbsolutePath().toString();
     }
 }
